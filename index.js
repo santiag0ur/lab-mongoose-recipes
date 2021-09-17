cbd9e63c88a02017933b22fd1ba0356d7c02145d
santiag0ur <83630273+santiag0ur@users.noreply.github.com>
2021-09-17 15:24:18 +0200

    done

--- index.js
+++ index.js
@@ -14,14 +14,56 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
+  // Run your code here, after you have insured that the connection was made
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log('Previous content of the database deleted');
+    return Recipe.create({
+      title: 'Cream Chip Cookies',
+      level: 'Amateur Chef',
+      ingredients: [
+        '1/2 cup light brown sugar',
+        '1 large egg',
+        '2 tablespoons milk',
+        '1 1/4 teaspoons vanilla extract',
+        '2 cups semisweet cream chips'
+      ],
+      cuisine: 'French',
+      dishType: 'dessert',
+      image:
+        'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4398987.jpg&w=596&h=399.32000000000005&c=sc&poi=face&q=85',
+      duration: 30,
+      creator: 'Chef Jennifer'
+    });
   })
-  .catch(error => {
+  .then((recipe) => {
+    console.log('A new recipe has been added to the database');
+    console.log(recipe);
+    return Recipe.insertMany(data);
+  })
+  .then((recipes) => {
+    console.log('New recipes have been added to the database');
+    console.log(recipes);
+    return Recipe.findOneAndUpdate(
+      { title: { $regex: 'Rigatoni' } },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then((recipe) => {
+    console.log('A recipe has been updated');
+    console.log(recipe);
+    return Recipe.deleteOne({ title: { $regex: 'Cake' } });
+  })
+  .then((recipe) => {
+    console.log('success!');
+    console.log(recipe);
+    mongoose.connection.close();
+  })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
